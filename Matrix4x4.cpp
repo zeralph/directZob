c5fd860f26df131c2a641800f79e3cbfc7808fce
zeralph <zeralph@gmail.com>
2019-06-07 22:13:54 +0200

    fucking matrix4x4

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -9,7 +9,10 @@
 
 static Matrix4x4 tmp;
 static Matrix4x4 tmpMul;
-static float identityArray[16] = { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f };
+static float identityArray[16] = {	1.0f, 0.0f, 0.0f, 0.0f, 
+									0.0f, 1.0f, 0.0f, 0.0f, 
+									0.0f, 0.0f, 1.0f, 0.0f, 
+									0.0f, 0.0f, 0.0f, 1.0f };
 Matrix4x4::Matrix4x4()
 {
 	Identity();
@@ -26,45 +29,87 @@ Matrix4x4::~Matrix4x4()
 
 void Matrix4x4::Mul(const Matrix4x4* m)
 {
-	//tmpMul.Identity();
-	tmpMul.m_data[0][0] = (m_data[0][0] * m->m_data[0][0]) +
-		(m_data[0][1] * m->m_data[1][0]) +
-		(m_data[0][2] * m->m_data[2][0]);
+	//
+	tmpMul.m_data[0][0] =	(m_data[0][0] * m->m_data[0][0]) +
+							(m_data[0][1] * m->m_data[1][0]) +
+							(m_data[0][2] * m->m_data[2][0]) +
+							(m_data[0][3] * m->m_data[3][0]);
 
-	tmpMul.m_data[1][0] = (m_data[1][0] * m->m_data[0][0]) +
-		(m_data[1][1] * m->m_data[1][0]) +
-		(m_data[1][2] * m->m_data[2][0]);
+	tmpMul.m_data[1][0] =	(m_data[1][0] * m->m_data[0][0]) +
+							(m_data[1][1] * m->m_data[1][0]) +
+							(m_data[1][2] * m->m_data[2][0]) +
+							(m_data[1][3] * m->m_data[3][0]);
 
-	tmpMul.m_data[2][0] = (m_data[2][0] * m->m_data[0][0]) +
-		(m_data[2][1] * m->m_data[1][0]) +
-		(m_data[2][2] * m->m_data[2][0]);
+	tmpMul.m_data[2][0] =	(m_data[2][0] * m->m_data[0][0]) +
+							(m_data[2][1] * m->m_data[1][0]) +
+							(m_data[2][2] * m->m_data[2][0]) +
+							(m_data[2][3] * m->m_data[3][0]);
 
+	tmpMul.m_data[3][0] =	(m_data[3][0] * m->m_data[0][0]) +
+							(m_data[3][1] * m->m_data[1][0]) +
+							(m_data[3][2] * m->m_data[2][0]) +
+							(m_data[3][3] * m->m_data[3][0]);
+	//
+	tmpMul.m_data[0][1] =	(m_data[0][0] * m->m_data[0][1]) +
+							(m_data[0][1] * m->m_data[1][1]) +
+							(m_data[0][2] * m->m_data[2][1]) +
+							(m_data[0][3] * m->m_data[3][1]);
 
-	tmpMul.m_data[0][1] = (m_data[0][0] * m->m_data[0][1]) +
-		(m_data[0][1] * m->m_data[1][1]) +
-		(m_data[0][2] * m->m_data[2][1]);
+	tmpMul.m_data[1][1] =	(m_data[1][0] * m->m_data[0][1]) +
+							(m_data[1][1] * m->m_data[1][1]) +
+							(m_data[1][2] * m->m_data[2][1]) +
+							(m_data[1][3] * m->m_data[3][1]);
 
-	tmpMul.m_data[1][1] = (m_data[1][0] * m->m_data[0][1]) +
-		(m_data[1][1] * m->m_data[1][1]) +
-		(m_data[1][2] * m->m_data[2][1]);
+	tmpMul.m_data[2][1] =	(m_data[2][0] * m->m_data[0][1]) +
+							(m_data[2][1] * m->m_data[1][1]) +
+							(m_data[2][2] * m->m_data[2][1]) +
+							(m_data[2][3] * m->m_data[3][1]);
 
-	tmpMul.m_data[2][1] = (m_data[2][0] * m->m_data[0][1]) +
-		(m_data[2][1] * m->m_data[1][1]) +
-		(m_data[2][2] * m->m_data[2][1]);
+	tmpMul.m_data[3][1] =	(m_data[3][0] * m->m_data[0][1]) +
+							(m_data[3][1] * m->m_data[1][1]) +
+							(m_data[3][2] * m->m_data[2][1]) +
+							(m_data[3][3] * m->m_data[3][1]);
+	//
+	tmpMul.m_data[0][2] =	(m_data[0][0] * m->m_data[0][2]) +
+							(m_data[0][1] * m->m_data[1][2]) +
+							(m_data[0][2] * m->m_data[2][2]) +
+							(m_data[0][2] * m->m_data[2][2]);
 
+	tmpMul.m_data[1][2] =	(m_data[1][0] * m->m_data[0][2]) +
+							(m_data[1][1] * m->m_data[1][2]) +
+							(m_data[1][2] * m->m_data[2][2]) +
+							(m_data[1][3] * m->m_data[3][2]);
 
-	tmpMul.m_data[0][2] = (m_data[0][0] * m->m_data[0][2]) +
-		(m_data[0][1] * m->m_data[1][2]) +
-		(m_data[0][2] * m->m_data[2][2]);
+	tmpMul.m_data[2][2] =	(m_data[2][0] * m->m_data[0][2]) +
+							(m_data[2][1] * m->m_data[1][2]) +
+							(m_data[2][2] * m->m_data[2][2]) +
+							(m_data[1][3] * m->m_data[3][2]);
 
-	tmpMul.m_data[1][2] = (m_data[1][0] * m->m_data[0][2]) +
-		(m_data[1][1] * m->m_data[1][2]) +
-		(m_data[1][2] * m->m_data[2][2]);
+	tmpMul.m_data[2][3] =	(m_data[3][0] * m->m_data[0][2]) +
+							(m_data[3][1] * m->m_data[1][2]) +
+							(m_data[3][2] * m->m_data[2][2]) +
+							(m_data[3][3] * m->m_data[3][2]);
+	//
+	tmpMul.m_data[3][0] =	(m_data[0][0] * m->m_data[0][3]) +
+							(m_data[0][1] * m->m_data[1][3]) +
+							(m_data[0][2] * m->m_data[2][3]) +
+							(m_data[1][2] * m->m_data[3][3]);
 
-	tmpMul.m_data[2][2] = (m_data[2][0] * m->m_data[0][2]) +
-		(m_data[2][1] * m->m_data[1][2]) +
-		(m_data[2][2] * m->m_data[2][2]);
+	tmpMul.m_data[3][1] =	(m_data[1][0] * m->m_data[0][3]) +
+							(m_data[1][1] * m->m_data[1][3]) +
+							(m_data[1][2] * m->m_data[2][3]) +
+							(m_data[1][3] * m->m_data[3][3]);
 
+	tmpMul.m_data[3][2] =	(m_data[2][0] * m->m_data[0][3]) +
+							(m_data[2][1] * m->m_data[1][3]) +
+							(m_data[2][2] * m->m_data[2][3]) +
+							(m_data[2][3] * m->m_data[2][3]);
+
+	tmpMul.m_data[3][3] =	(m_data[3][0] * m->m_data[0][3]) +
+							(m_data[3][1] * m->m_data[1][3]) +
+							(m_data[3][2] * m->m_data[2][3]) +
+							(m_data[3][3] * m->m_data[2][3]);
+	//
 	memcpy(&m_data, &tmpMul.m_data, sizeof(tmpMul.m_data));
 }
 
@@ -80,11 +125,13 @@ void Matrix4x4::CopyFrom(const Matrix4x4* m)
 
 void Matrix4x4::Mul(Vector3* v) const
 {
-	float fx = m_data[0][0] * v->x + m_data[0][1] * v->y + m_data[0][2] * v->w;
-	float fy = m_data[1][0] * v->x + m_data[1][1] * v->y + m_data[1][2] * v->w;
-	float fw = m_data[2][0] * v->x + m_data[2][1] * v->y + m_data[2][2] * v->w;
+	float fx = m_data[0][0] * v->x + m_data[0][1] * v->y + m_data[0][2] * v->z + m_data[0][3] * v->w;
+	float fy = m_data[1][0] * v->x + m_data[1][1] * v->y + m_data[1][2] * v->z + m_data[1][3] * v->w;
+	float fz = m_data[2][0] * v->x + m_data[2][1] * v->y + m_data[2][2] * v->z + m_data[2][3] * v->w;
+	float fw = m_data[3][0] * v->x + m_data[3][1] * v->y + m_data[3][2] * v->z + m_data[3][3] * v->w;
 	v->x = fx;// +m_translationX;
 	v->y = fy;// +m_translationY;
+	v->z = fz;
 	v->w = fw;
 }
 
