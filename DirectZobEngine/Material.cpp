50a62fa446c59aa28ec53407216b770b49ab9f38
zeralph <zeralph@gmail.com>
2020-04-28 13:50:35 +0200

    TGa et fbx mostly ok

--- DirectZobEngine/Material.cpp
+++ DirectZobEngine/Material.cpp
@@ -17,7 +17,7 @@ Material::Material(const std::string& name, const Vector3* ambientColor, const V
 	{
 		std::vector<unsigned char> image; //the raw pixels
 		unsigned width, height;
-		unsigned error = 0;
+		uint8_t error = 0;
 		if (textureFile)
 		{
 			std::string texFile = std::string(textureFile);
@@ -121,28 +121,61 @@ Material::Material(const std::string& name, const Vector3* ambientColor, const V
 			else if (texFile.find(".tga") != -1)
 			{
 				int w, h, bpp = 0;
-				uint8_t* d = tga_load(textureFile, &w, &h, &bpp);
-				if (d)
+				tga::ImageFormat format = tga::ImageFormat::Undefined;
+				tga::TGA tgaTex;
+				tgaTex.Load(textureFile);
+				//uint8_t* d = NULL;// tga_load(textureFile, &w, &h, &bpp, &error);
+				//if (error == 0 && d)
+				if(tgaTex.Load(textureFile))
 				{
-					int size = w * h;
-					m_data = (float*)malloc(sizeof(float) * 4 * size);
-					int j = 0;
-					for (int i = 0; i < size; i += 3)
+					w = tgaTex.GetWidth();
+					h = tgaTex.GetHeight();
+					format = tgaTex.GetFormat();
+					if (format == tga::ImageFormat::RGB)
 					{
-						float g = (float)d[i] / 255.0f;
-						float b = (float)d[i + 1] / 255.0f;
-						float r = (float)d[i + 2] / 255.0f;
-						//float b = (float)d[i + 3] / 255.0f;
-						m_data[j] = r;
-						m_data[j + 1] = g;
-						m_data[j + 2] = b;
-						m_data[j + 3] = 1.0f;
+						bpp = 3;
+					}
+					else if (format == tga::ImageFormat::RGBA)
+					{
+						bpp = 4;
+					}
+					else if (format == tga::ImageFormat::Monochrome)
+					{
+						return;// bpp = 1;
+					}
+					else
+					{
+						return;
+					}
+					uint8_t* d = tgaTex.GetData();
+					uint64_t s = tgaTex.GetSize();
+					m_data = (float*)malloc(sizeof(float) * w*h*4);
+					int j = 0;
+					for (int i = 0; i < s; i += bpp)
+					{
+						float r = (float)d[i] / 255.0f;
+						float g = (float)d[i + 1] / 255.0f;
+						float b = (float)d[i + 2] / 255.0f;
+						float a = 1.0f;
+						if (r == 0.0f || g == 0.0f || b == 0.0f)
+						{
+							int hh = 0;
+							hh++;
+						}
+						if (bpp == 4)
+						{
+							a = (float)image[i + 3] / 255.0f;
+						}
+						m_data[j] = b;
+						m_data[j + 1] =r;
+						m_data[j + 2] = g;
+						m_data[j + 3] = a;
 						j += 4;
 					}
 					m_width = w;
 					m_height = h;
-					m_dataSize = w * h;
-					delete d;
+					m_dataSize = width * height * 4;
+					//delete d;
 				}
 				else
 				{
