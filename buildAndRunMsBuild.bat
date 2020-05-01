8c77a388bbf9f8ca2714631ad18c36c92ba25c14
punkyman <julien.reiss@gmail.com>
2020-05-01 10:17:42 +0200

    change to build & run

---
+++ buildAndRunMsBuild.bat
@@ -0,0 +1,7 @@
+@echo off
+pushd DirectZobEngine
+CALL generate_sln.bat
+python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
+msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1
+popd
+start /W DirectZobEngine\build\DirectZob\Debug\DirectZobExe.exe .\resources\
--- buildMsBuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd
-cd DirectZobEngine
-CALL generate_sln.bat
-python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
-msbuild -m build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1
-popd
