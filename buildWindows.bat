7c34dff554cde1a43a3454f858af8c3493a0d6e6
zeralph <zeralph@gmail.com>
2020-05-02 14:42:13 +0200

    windows script build

---
+++ buildWindows.bat
@@ -0,0 +1,6 @@
+@echo off
+pushd DirectZobEngine
+CALL generate_sln.bat
+python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
+msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1
+popd
