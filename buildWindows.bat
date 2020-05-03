d9e4ac7d1bcac2776362c3160b3051631d54c06f
zeralph <zeralph@gmail.com>
2020-05-03 13:36:44 +0200

    buildWindows.bat includes target

--- buildWindows.bat
+++ buildWindows.bat
@@ -5,5 +5,5 @@ SET target=Release
 IF %1.!=.
 SET target=%1
 python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
-msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1 -target:%Release%
+msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1 -target:%target%
 popd
