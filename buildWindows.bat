5fa0b8c6d39a32d53fabb2e43bd3973c4fa55f35
zeralph <zeralph@gmail.com>
2020-05-03 13:38:07 +0200

    buildwindows.bat merdouille

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,9 +1,8 @@
 @echo off
 pushd DirectZobEngine
 CALL generate_sln.bat
-SET target=Release
-IF %1.!=.
 SET target=%1
+IF [%1]==[] SET target=Release
 python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
 msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1 -target:%target%
 popd
