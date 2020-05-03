d553ab89bd1ae416e9e5dfc202b986a0ab49ed51
zeralph <zeralph@gmail.com>
2020-05-03 12:40:13 +0200

    vscode windows debug and build

--- buildWindows.bat
+++ buildWindows.bat
@@ -1,6 +1,9 @@
 @echo off
 pushd DirectZobEngine
 CALL generate_sln.bat
+SET target=Release
+IF %1.!=.
+SET target=%1
 python.exe .\strip_emptylines.py .\build\DirectZob\DirectZob.sln
-msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1
+msbuild build\DirectZob\DirectZob.sln /p:TargetFrameworkVersion=v4.6.1 -target:%Release%
 popd
