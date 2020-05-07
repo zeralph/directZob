be1b64e1209fe5caf74c2b3f86fd91547fa49f3c
zeralph <zeralph@gmail.com>
2020-05-07 08:41:16 +0200

    python script strip_emptryline compatible with both python 2 & 3

--- DirectZobEngine/strip_emptylines.py
+++ DirectZobEngine/strip_emptylines.py
@@ -1,20 +1,33 @@
 import sys
 import os.path
+import platform
 
-UNIX_NEWLINE = '\n'
-WINDOWS_NEWLINE = '\r\n'
-
-filePath = sys.argv[1]
-if not os.path.exists(filePath):
-    print("cannot open " + filePath)
+version = platform.python_version()
+majorVersion = version[0]
+if majorVersion is '2':
+    print('running in pyhton 2')
+    UNIX_NEWLINE = '\n'
+    WINDOWS_NEWLINE = '\r\n'
+    filePath = sys.argv[1]
+    if not os.path.exists(filePath):
+        print("cannot open " + filePath)
+    else:
+        file = open(filePath)
+        content = ''
+        for line in file: 
+            lines = line.replace(WINDOWS_NEWLINE, UNIX_NEWLINE) 
+            content = content + lines
+        file.close()
+        file = open(filePath, 'w')
+        file.write(content)
+        file.close()
 else:
+    print('running in pyhton 3')
+    filePath = sys.argv[1]
     file = open(filePath)
     content = ''
-    for line in file: 
-        lines = line.replace(WINDOWS_NEWLINE, UNIX_NEWLINE) 
-        content = content + lines
-    file.close()
+    for line in file:
+        if not line.strip(): continue  # skip the empty line
+        content = content + line
     file = open(filePath, 'w')
     file.write(content)
-    file.close()
-
