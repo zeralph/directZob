f35530c7e598a445ff23a61aef4826363e9506be
zeralph <zeralph@gmail.com>
2020-05-09 18:08:04 +0200

    linux and mac buildfix

--- DirectZobEngine/DirectZob.h
+++ DirectZobEngine/DirectZob.h
@@ -1,30 +1,4 @@
 #pragma once
-
-
-#ifdef LINUX
-    #define MAX_PATH 256
-    #include <unistd.h>
-	#define SLEEP(a) sleep(a/1000)
-	#define _snprintf_s(a,b,c,...) snprintf(a,b,c,__VA_ARGS__)
-	#define _vsnprintf_s(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
-    #define fopen_s(fp, fmt, mode)  ({\
-        *(fp)=fopen( (fmt), (mode));\
-        (*(fp) ) ? 0:errno;\
-    })
-#elif WINDOWS
-	#include <windows.h>
-	#define SLEEP(a) Sleep(a)
-#elif MACOS
-    #define MAX_PATH 256
-    #include <unistd.h>
-	#define SLEEP(a) sleep(a/1000)
-	#define _snprintf_s(a,b,c,...) snprintf(a,b,c,__VA_ARGS__)
-	#define _vsnprintf_s(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
-	#define fopen_s(fp, fmt, mode)  ({\
-        *(fp)=fopen( (fmt), (mode));\
-        (*(fp) ) ? 0:errno;\
-    })
-#endif //LINUX
 #include <string>
 #include <iostream>
 #include <algorithm>
