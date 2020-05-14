71d3f28d63d9b6b60039bb888da2b118b74e393e
lucas richalland <zeralph@hotmail.fr>
2020-05-14 12:44:52 +0200

    smoke test windows bat

---
+++ smokeTestWindows.bat
@@ -0,0 +1,11 @@
+SET target=%1
+IF [%target%]==[] ( 
+     SET target=Release
+)
+echo *** SMOKE TEST TARGET %target% ***
+pushd DirectZobEngine
+XCOPY .\build\DirectZob\%target%\DirectZobExe.exe .. /Y
+XCOPY .\build\DirectZob\%target%\DirectZobExe.pdb .. /Y
+XCOPY .\build\DirectZob\%target%\libfbxsdk.dll .. /Y
+popd
+start /W DirectZobEngine\build\DirectZob\Release\DirectZobExe.exe --scene .\resources\fbx.xml --bench --test
