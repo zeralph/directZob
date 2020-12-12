2a20cd6dfee10b36ffef270347164bcc4b907260
zeralph <zeralph@gmail.com>
2020-12-12 18:04:55 +0100

    fixes again for macos linux

--- DirectZobEngine/Types.h
+++ DirectZobEngine/Types.h
@@ -15,6 +15,7 @@
 	#define _TRUNCATE ((size_t)-1)
 	#define _snprintf_s(a,b,c,...) snprintf(a,b,c,__VA_ARGS__)
 	#define _vsnprintf(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
+	#define _vsnprintf_s(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
 	#define _vscprintf(a,...) snprintf(NULL,0,a,__VA_ARGS__)
     #define fopen_s(fp, fmt, mode)  ({\
         *(fp)=fopen( (fmt), (mode));\
@@ -30,6 +31,7 @@
 	#define _TRUNCATE ((size_t)-1)
 	#define _snprintf_s(a,b,c,...) snprintf(a,b,c,__VA_ARGS__)
 	#define _vsnprintf(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
+	#define _vsnprintf_s(a,b,c,...) vsnprintf(a,b,c,__VA_ARGS__)
 	#define _vscprintf(a,...) snprintf(NULL,0,a,__VA_ARGS__)
 	#define fopen_s(fp, fmt, mode)  ({\
         *(fp)=fopen( (fmt), (mode));\
