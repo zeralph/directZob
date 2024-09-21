7aa0781ba2b6da63ff1617c2a590879da9fe829a
zeralph <zeralph@gmail.com>
2024-09-21 09:38:52 +0200

    ps1 for windows generation

---
+++ DirectZobEngine/generate_sln.ps1
@@ -0,0 +1,6 @@
+
+pip3 install gyp-next
+gyp .\DirectZob.gyp --depth=. --generator-output=build/DirectZob/ -f msvs -G msvs_version=2022
+
+
+(Get-Content .\build\DirectZob\DirectZobDInterop.vcxproj).Replace('<RootNamespace>DirectZobDInterop</RootNamespace>', '<RootNamespace>DirectZobDInterop</RootNamespace><ManagedAssembly>true</ManagedAssembly>') | Set-Content .\build\DirectZob\DirectZobDInterop.vcxproj
